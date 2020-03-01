8bf5838212a936b9636238b06e97670a9d694671
kmbarnett <kmbarnett94@gmail.com>
2020-03-01 13:16:58 -0700

    Fixed tests to run with changes

--- test/User-test.js
+++ test/User-test.js
@@ -4,13 +4,15 @@ const spies = require('chai-spies');
 chai.use(spies);
 const should = chai.should()
 
-// import Agent from '../Agent.js'
-// import Client from '../Client.js'
+import Agent from '../src/Agent.js'
+import Client from '../src/Client.js'
 
 const expect = chai.expect;
 
 describe('User', function() {
   let user;
+  let userData;
+  let tripsData
   global.window = {}
 
   chai.spy.on(window, 'fetch', () => {
@@ -21,6 +23,44 @@ describe('User', function() {
 
 
   beforeEach(function() {
+    tripsData =
+    [{
+      id: 1,
+      userID: 1,
+      destinationID: 1,
+      travelers: 1,
+      date: "2019/09/16",
+      duration: 8,
+      status: "approved",
+      suggestedActivities: [ ]
+    },
+    {
+      id: 2,
+      userID: 1,
+      destinationID: 2,
+      travelers: 5,
+      date: "2020/10/04",
+      duration: 18,
+      status: "pending",
+      suggestedActivities: [ ]
+    },
+    {
+      id: 3,
+      userID: 1,
+      destinationID: 3,
+      travelers: 4,
+      date: "2020/05/22",
+      duration: 17,
+      status: "pending",
+      suggestedActivities: [ ]
+    }, ];
+
+    userData = {
+      id: 1,
+      name: "Ham Leadbeater",
+      travelerType: "relaxer"
+    }
+
     user = new User()
   });
 
@@ -28,10 +68,14 @@ describe('User', function() {
   it('Should be a function', function() {
     expect(User).to.be.a('function');
   });
-  describe('Log in', function(){
+  describe('Log in', function() {
 
-    it('Should be able log i n a client', function() {
-      expect(user.userLogIn(1)).to.equal();
+    it('Should be able log in a client', function() {
+      expect(user.showClient(userData, tripsData)).to.be.instanceof(Client);
+    });
+
+    it('Should be able log in a agent', function() {
+      expect(user.showAgent(tripsData)).to.be.instanceof(Agent);
     });
 
   });
