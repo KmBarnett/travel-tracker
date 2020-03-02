dafc092cb45a2dfd6a6e2b407da408ede131c764
kmbarnett <kmbarnett94@gmail.com>
2020-03-02 07:05:50 -0700

    Add functionality to prevent wrong formatting

--- src/Data-Controller.js
+++ src/Data-Controller.js
@@ -1,4 +1,4 @@
-
+import moment from 'moment'
 
 
 const dataController = {
@@ -10,10 +10,23 @@ const dataController = {
     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
       .then(response => response.json())
       .then(data => dataController.trips = data.trips)
+      .then(dataController.formatDate)
       .catch(err => console.log(err.message))
   },
 
+  formatDate() {
+    dataController.trips.forEach(trip => {
+      let date = trip.date.split('-')
+      if (date.length > 1) {
+        trip.date = `${date[0]}/${date[1]}/${date[2]}`
+      }
+    });
+
+
+  },
+
   dateToNum(date) {
+
     let splitDate = date.split('/')
     if (splitDate[1].length === 1) {
       splitDate[1] = `0${splitDate[1]}`
@@ -21,12 +34,24 @@ const dataController = {
     return parseInt(splitDate.join(''))
   },
 
+  compareDates(tripsDate) {
+    let today = moment().format('YYYY/MM/DD')
+    return dataController.dateToNum(today) > dataController.dateToNum(tripsDate)
+  },
+
   grabUserTrips(id) {
     let trips = dataController.trips.filter(trip => trip.userID === id * 1)
     trips.sort((a, b) => {
-      return (dataController.dateToNum(a.date) - dataController.dateToNum(b.date))
+      return dataController.dateToNum(a.date) - dataController.dateToNum(b.date)
     })
-    console.log(trips);
+    return trips
+  },
+
+  grabAdminTrips() {
+    let trips = [...dataController.trips]
+    trips.sort((a, b) => {
+      return dataController.dateToNum(a.date) - dataController.dateToNum(b.date)
+    })
     return trips
   },
 
