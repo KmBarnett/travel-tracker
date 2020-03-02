97a1418ae1a99c6686823d251864253081691faa
kmbarnett <kmbarnett94@gmail.com>
2020-03-02 07:02:40 -0700

    Add functionality to show all user trips and compare date so that only trips that are pending and in the future can be pending

--- src/Agent.js
+++ src/Agent.js
@@ -3,15 +3,37 @@ import Client from './Client.js';
 class Agent extends Client {
   constructor(userData, tripsData) {
     super({name: 'Mr. Jackson'}, tripsData)
-    this.users = userData;
+    this.users = userData.travelers;
   }
 
-  listPendingTrips() {
-    return this.trips.filter(trip => trip.status === 'pending')
+  compareDates(dates) {
+    let numDates = dates.map(date => {
+      let splitDate = date.split('/')
+      if (splitDate[1].length === 1) {
+        splitDate[1] = `0${splitDate[1]}`
+      }
+      return parseInt(splitDate.join(''))
+    })
+    return numDates[0] >= numDates[1]
+  }
+
+  listPendingTrips(date) {
+    return this.trips.filter(trip =>  {
+      let pending = trip.status === 'pending'
+      let future = this.compareDates([trip.date, date])
+      return (pending && future)
+    })
+  }
+
+  listTripsPendingFirst() {
+    let shallowTrips = [...this.trips]
+     shallowTrips.sort(a =>  {
+      return (a.status === 'pending') ? -1 : 1;
+    })
+    return shallowTrips
   }
 
   showTravelCount(date) {
-    console.log(date);
     let totalTrips = this.trips.filter(trip => trip.date === date)
     return totalTrips.length
   }
